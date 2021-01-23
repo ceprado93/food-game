6ef6e3a9d0ea919fa57455409ed26893feb62954
montse <montserrat.mosqueda@gmail.com>
2021-01-23 11:22:54 +0100

    commit

--- proyecto/gamePage/js/chef.js
+++ proyecto/gamePage/js/chef.js
@@ -1,5 +1,37 @@
-class chef{
+class Chef{
     // sera el personaje principal, tiene que recolectar los platos que se le indican en el tablero
     // moviendose de izq a drcha, tiene que evitar a chikote saltandolo
     // y esquivar el jagger
-}
+
+        constructor(ctx, chefPosX, chefPosY, chefWidth, chefHeight, chefImage) {
+             this.ctx = ctx
+     
+             this.chefPos = {
+                 x: chefPosX,
+                 y: chefPosY
+             }
+             this.chefSize = {
+                 w: chefWidth,
+                 h: chefHeight
+             }
+             this.imageName = chefImage
+             this.chefInstance = undefined
+             this.init()
+         }
+     
+         init() {
+             this.chefInstance = new Image()
+             this.chefInstance.src = '/images/mariokart.png'
+         }
+     
+         draw() {
+           this.ctx.drawImage(this.chefInstance, this.chefPos.x, this.chefPos.y, this.chefSize.w, this.chefSize.h)
+         }
+     
+         move(dir) {
+             dir === 'left' && this.chefPos.x > 50 ? this.chefPos.x -= 20 : null
+             dir === 'right' && this.chefPos.x < (450-this.chefSize.w) ? this.chefPos.x += 20 : null
+             dir === 'up'&& this.chefPos.y > 50 ? this.chefPos.y -= 20 : null
+         }
+     
+      }
--- proyecto/gamePage/js/game.js
+++ proyecto/gamePage/js/game.js
@@ -10,6 +10,7 @@ const lunchGame = {
     license: undefined,
     /** @type {CanvasRenderingContext2D} */
     ctx: undefined,
+    chef: undefined,
     canvasDom: undefined,
     canvasSize: {
         w: undefined,
@@ -20,14 +21,65 @@ const lunchGame = {
         this.canvasDom = document.querySelector (`#${id}`)
         this.ctx = this.canvasDom.getContext ('2d')
         this. setDimensions()
+        this.drawBoard()
+        this.drawAll()
     },
 
     setDimensions(){
         this.canvasSize = {
-            w: window.innerWidth,
-            h: window.innerHeight
+            w: window.innerWidth-600,
+            h: window.innerHeight,
+            PosX:200
         }
         this.canvasDom.setAttribute ('width', this.canvasSize.w)
         this.canvasDom.setAttribute ('height', this.canvasSize.h)
+        //this.canvasDom.setAttribute ()
+
+        console.log(this.canvasDom)
     },
+
+    drawBoard() {
+        this.ctx.fillStyle = 'white'
+        this.ctx.fillRect(0, 0, this.canvasSize.w, this.canvasSize.h)
+ },
+ 
+ createChef() {
+    this.chef = new Chef (this.ctx, 150, 550, 100, 100, 'mariokart.png')
+  },
+
+  setEventListeners() {
+    document.onkeydown = e => {
+
+      if (e.key === this.keys.left){
+        this.chef.move('left')
+      }
+      if (e.key === this.keys.right) {
+        this.chef.move('right')
+      }
+    }
+    document.onkeypress = e => {
+        
+        this.chef.move('space')
+        console.log (e)
+    }
+  },
+
+  drawAll() {
+    setInterval(() => {
+       // this.frames++
+        // this.frames % 70 === 0 ? this.createObstacle() : null
+        // this.clearScreen()
+        // this.drawRoad()
+        // this.drawDashedLines()
+        // this.drawContinuousLines()
+        this.chef.draw()
+        // this.obstacle.drawObst() //? this.createObstacle() : null REVISAR
+        // this.totalScore() //REVISAR---------------------------------------------
+    }, 70)
+},
+
+clearScreen() {
+    this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
+},
+
 }
